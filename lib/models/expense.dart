ab08fd04e780343970134edb49578688936f4b5c
itrieditlikethis <albert.fedev@gmail.com>
2025-07-31 17:17:59 +0300

    fix: expense model id

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -3,12 +3,8 @@ import 'package:uuid/uuid.dart';
 var uuid = Uuid();
 
 class Expense {
-  Expense({
-    required this.id,
-    required this.title,
-    required this.amount,
-    required this.date,
-  });
+  Expense({required this.title, required this.amount, required this.date})
+    : id = uuid.v4();
 
   final String id;
   final String title;
