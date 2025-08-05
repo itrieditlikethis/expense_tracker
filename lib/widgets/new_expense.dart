ab2156365425305daea8b95e50480133a0c70b7c
itrieditlikethis <albert.fedev@gmail.com>
2025-08-05 18:43:08 +0300

    feat: add input text controller

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,6 +10,14 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _addNewExpenseController = TextEditingController();
+
+  @override
+  void dispose() {
+    _addNewExpenseController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -21,6 +29,7 @@ class _NewExpenseState extends State<NewExpense> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             TextField(
+              controller: _addNewExpenseController,
               maxLength: 50,
               decoration: InputDecoration(label: Text("what expense?")),
               keyboardType: TextInputType.text,
