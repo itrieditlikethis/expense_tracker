47d8f964eace5175a5752d151296580873b68862
itrieditlikethis <albert.fedev@gmail.com>
2025-08-06 13:44:56 +0300

    feat: add amount input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -11,10 +11,14 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _addNewExpenseController = TextEditingController();
+  final _addAmmountController = TextEditingController();
+
+  // final _expenseDataMap = {};
 
   @override
   void dispose() {
     _addNewExpenseController.dispose();
+    _addAmmountController.dispose();
     super.dispose();
   }
 
@@ -33,6 +37,27 @@ class _NewExpenseState extends State<NewExpense> {
               maxLength: 50,
               decoration: InputDecoration(label: Text("what expense?")),
               keyboardType: TextInputType.text,
+            ),
+            TextField(
+              controller: _addAmmountController,
+              maxLength: 6,
+              decoration: InputDecoration(
+                label: Text('amount'),
+                prefixText: '\$ ',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () => print(
+                    '${_addNewExpenseController.text} / ${_addAmmountController.text}',
+                  ),
+                  child: Text('Save'),
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Cancel")),
+              ],
             ),
           ],
         ),
